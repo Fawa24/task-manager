417909c190bc18153c8ce83a26ca261d304fcd6d
Fawa24 <ofeglol@gmail.com>
2024-05-26 20:03:49 +0300

    Add TaskCard's DTOs

---
+++ Api/Api/Core/DTO/TaskCardAddRequest.cs
@@ -0,0 +1,32 @@
+﻿using Api.Core.Domain.Entities;
+
+namespace Api.Core.DTO
+{
+	public class TaskCardAddRequest
+	{
+		public string? Name { get; set; }
+
+		public string? Description { get; set; }
+
+		public DateOnly? DueDate { get; set; }
+
+		public string? Priority { get; set; }
+
+		public Guid? Listid { get; set; }
+
+		public virtual TaskList? List { get; set; }
+
+		public TaskCard ToTaskCard()
+		{
+			return new TaskCard() {
+				Taskid = Guid.NewGuid(),
+				Name = Name,
+				Description = Description,
+				DueDate = DueDate,
+				Priority = Priority,
+				Listid = Listid
+				//TO DO: figure out how to assign the List property a value via ListId
+			};
+		}
+	}
+}
---
+++ Api/Api/Core/DTO/TaskCardResponce.cs
@@ -0,0 +1,38 @@
+﻿using Api.Core.Domain.Entities;
+
+namespace Api.Core.DTO
+{
+	public class TaskCardResponce
+	{
+		public Guid Taskid { get; set; }
+
+		public string? Name { get; set; }
+
+		public string? Description { get; set; }
+
+		public DateOnly? DueDate { get; set; }
+
+		public string? Priority { get; set; }
+
+		public Guid? Listid { get; set; }
+
+		public virtual TaskList? List { get; set; }
+	}
+
+	public static class TaskCardExtension
+	{
+		public static TaskCardResponce ToTaskCardResponce(this TaskCard taskCard)
+		{
+			return new TaskCardResponce
+			{
+				Taskid = taskCard.Taskid,
+				Name = taskCard.Name,
+				Description = taskCard.Description,
+				DueDate = taskCard.DueDate,
+				Priority = taskCard.Priority,
+				Listid = taskCard.Listid,
+				List = taskCard.List
+			};
+		}
+	}
+}

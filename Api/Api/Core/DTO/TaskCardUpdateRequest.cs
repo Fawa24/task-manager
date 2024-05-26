af65ddc9961c69b3debc7cf0693db9cff8bb1a6b
Fawa24 <ofeglol@gmail.com>
2024-05-26 20:12:45 +0300

    Add TaskCardUpdate DTO

---
+++ Api/Api/Core/DTO/TaskCardUpdateRequest.cs
@@ -0,0 +1,34 @@
+﻿using Api.Core.Domain.Entities;
+
+namespace Api.Core.DTO
+{
+	public class TaskCardUpdateRequest
+	{
+		public Guid Taskid { get; set; }
+
+		public string? Name { get; set; }
+
+		public string? Description { get; set; }
+
+		public DateOnly? DueDate { get; set; }
+
+		public string? Priority { get; set; }
+
+		public Guid? Listid { get; set; }
+
+		public virtual TaskList? List { get; set; }
+
+		public TaskCard ToTaskCard()
+		{
+			return new TaskCard()
+			{
+				Taskid = Taskid,
+				Name = Name,
+				Description = Description,
+				DueDate = DueDate,
+				Priority = Priority,
+				Listid = Listid
+			};
+		}
+	}
+}

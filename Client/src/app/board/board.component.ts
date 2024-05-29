78d55f1e21c910d3bde1d0a8f79a93a94dabbe7c
Fawa24 <ofeglol@gmail.com>
2024-05-29 11:43:50 +0300

    Implement board component

--- Client/src/app/board/board.component.ts
+++ Client/src/app/board/board.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../models/task-list';
+import { TaskService } from '../Task/task.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrl: './board.component.css'
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit {
+  lists: TaskList[] = []
 
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTaskLists().subscribe(data => {
+      this.lists = data
+    })
+  }
 }

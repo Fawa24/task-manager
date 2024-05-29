37f35a5392ada5116b1591b8106672b5b7f03e24
Fawa24 <ofeglol@gmail.com>
2024-05-29 11:43:22 +0300

    Add GetAllLists method to TaskService

--- Client/src/app/Task/task.service.ts
+++ Client/src/app/Task/task.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { TaskList } from '../models/task-list';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  taskLists: TaskList[] = []
 
-  constructor() { }
+  private apiUrl: string = "https://localhost:7012"
+  constructor(private http: HttpClient) { }
+
+  getTaskLists() : Observable<TaskList[]> {
+    return this.http.get<TaskList[]>(this.apiUrl + "/lists")
+  }
 }
